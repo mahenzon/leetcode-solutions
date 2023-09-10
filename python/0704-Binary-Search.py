f24e43a6386da3da75d6949ab9683087e96f3e18
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-09-10 22:18:43 +0300

    Add Python solution for 0704 Binary Search

---
+++ python/0704-Binary-Search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            # mid = (left + right) // 2
+            mid = left + ((right - left) // 2)
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+
+        return -1
