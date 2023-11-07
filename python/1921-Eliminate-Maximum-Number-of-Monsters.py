35c8c8cd870b5980af86bae39e8b8686e3fe1c7d
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-11-07 23:20:34 +0300

    Add Python solution for 1921 Eliminate Maximum Number of Monsters

---
+++ python/1921-Eliminate-Maximum-Number-of-Monsters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def eliminateMaximum(self, dist: list[int], speed: list[int]) -> int:
+        arrival_times = sorted([
+            (distance - 1) // monster_speed
+            for distance, monster_speed in zip(dist, speed)
+        ])
+        for step, arrival_time in enumerate(arrival_times):
+            if step > arrival_time:
+                return step - 1
+        return len(dist)
