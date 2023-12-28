94e01f9b2f3182247966e892eac232370654aa24
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-12-28 22:50:01 +0300

    Update Python solution for 0674 Longest Continuous Increasing Subsequence

--- python/0674-Longest-Continuous-Increasing-Subsequence.py
+++ python/0674-Longest-Continuous-Increasing-Subsequence.py
@@ -1,9 +1,10 @@
-# Longer code, faster solution
-# Runtime 58 ms Beats 99.22%
-# Memory 18.7 MB Beats 11.15%
-
-
 class Solution:
+    """
+    Longer code, faster solution
+    Runtime 58 ms Beats 99.22%
+    Memory 18.7 MB Beats 11.15%
+    """
+
     def findLengthOfLCIS(self, nums: List[int]) -> int:
         res = 1
         count = 1
@@ -18,12 +19,13 @@ class Solution:
         return res
 
 
-# Shorted code, slower solution
-# Runtime 129 ms Beats 6.9%
-# Memory 18.8 MB Beats 8.82%
-
-
 class Solution:
+    """
+    Shorted code, slower solution
+    Runtime 129 ms Beats 6.9%
+    Memory 18.8 MB Beats 8.82%
+    """
+
     def findLengthOfLCIS(self, nums: List[int]) -> int:
         res = 1
         last = 0
