6ff8200e4df6e72645bb5c2ad38499e9c5c708a4
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-01-07 19:46:27 +0300

    Update Python solution for 2353 Design a Food Rating System

--- python/2353-Design-a-Food-Rating-System.py
+++ python/2353-Design-a-Food-Rating-System.py
@@ -1,8 +1,33 @@
+# If you're OK with importing SortedSet:
+from sortedcontainers import SortedSet
+
+
+class FoodRatings:
+    def __init__(self, foods: list[str], cuisines: list[str], ratings: list[int]):
+        self.foods = {}
+        self.cuisines = defaultdict(SortedSet)
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.cuisines[cuisine].add((-rating, food))
+            self.foods[food] = (cuisine, rating)
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine, rating = self.foods[food]
+
+        self.cuisines[cuisine].remove((-rating, food))
+        self.cuisines[cuisine].add((-newRating, food))
+
+        self.foods[food] = (cuisine, newRating)
+
+    def highestRated(self, cuisine: str) -> str:
+        return self.cuisines[cuisine][0][1]
+
+
 # `defaultdict` and `bisect` helpers are already imported in LeetCode
 from collections import defaultdict
 from bisect import bisect_left, insort
 
 
+# using bisect (slower because of O(n) time for .pop and .insert (insort)
 class FoodRatings:
     def __init__(self, foods: list[str], cuisines: list[str], ratings: list[int]):
         self.foods = {}
