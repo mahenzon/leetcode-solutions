96a64e15dfbd6108bc7ffe1f19b42db364daddba
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-03 22:49:25 +0300

    Add Python solution for 1845 Seat Reservation Manager

--- python/1845-Seat-Reservation-Manager.py
+++ python/1845-Seat-Reservation-Manager.py
@@ -12,8 +12,3 @@ class SeatManager:
 
     def unreserve(self, seatNumber: int) -> None:
         heapq.heappush(self.available_seats, seatNumber)
-
-# Your SeatManager object will be instantiated and called as such:
-# obj = SeatManager(n)
-# param_1 = obj.reserve()
-# obj.unreserve(seatNumber)
