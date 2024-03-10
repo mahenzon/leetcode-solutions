dd2b23e4881f196240158265a25691b3c0e9b49b
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-10 11:27:54 +0300

    Add another Python solution for 0349 Intersection of Two Arrays

--- python/0349-Intersection-of-Two-Arrays.py
+++ python/0349-Intersection-of-Two-Arrays.py
@@ -1,5 +1,6 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # return set(nums1).intersection(nums2)
         known = set(nums1)
         result = []
         for num in nums2:
