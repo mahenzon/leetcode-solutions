3d4d7f15fe4dfe114a10698924dbf9665c8bf7c4
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-03-24 18:28:47 +0300

    Add Python solution for 0287 Find the Duplicate Number

---
+++ python/0287-Find-the-Duplicate-Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[nums[0]]
+        fast = nums[slow]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
