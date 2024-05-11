81bc4764ee02e4a0a485bc78fa02d864fdb4cd88
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-05-11 22:10:46 +0300

    Update Python solution for 0268 Missing Number

--- python/0268-Missing-Number.py
+++ python/0268-Missing-Number.py
@@ -4,9 +4,4 @@ class Solution:
         # [0, 1, 2, 3] -> sum = 6
 
         # [0, 1, 3] -> sum = 4, missing 2
-        # total present: 3
-        # 3 + (idx num sum) = 3 + sum(range(len(nums))) = 3 + (0 + 1 + 2) = 6
-        # 6 - 0 - 1 - 3 = 2 <- result
-        # range(len(nums)) is range for [0, n) where n is number if nums
-        total = len(nums) + sum(range(len(nums)))
-        return total - sum(nums)
+        return sum(range(len(nums) + 1)) - sum(nums)
