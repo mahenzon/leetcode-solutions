6d27a39b21c18322479b3182daf1efcc22976e87
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-06-13 09:53:46 +0300

    Add Python solution for 2037 Minimum Number of Moves to Seat Everyone

---
+++ python/2037-Minimum-Number-of-Moves-to-Seat-Everyone.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+
+        return sum(
+            abs(a - b)
+            for a, b in zip(seats, students)
+        )
