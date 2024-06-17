f7303d147a8d61977d660fad1082f41d5a385c7d
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-06-17 23:51:26 +0300

    Update Python solution for 0052 N-Queens II

--- python/0052-N-Queens-II.py
+++ python/0052-N-Queens-II.py
@@ -5,42 +5,42 @@ class Solution:
             return
 
         for col in range(self.n):
-            pos_diagonal = row + col
-            neg_diagonal = row - col
-            # check place can be used
+            pos_idx = row + col
+            neg_idx = row - col
+
             if (
-                # col is occupied
                 col in self.occupied_cols
-                # pos diagonal is occupied
-                or pos_diagonal in self.occupied_pos_diagonal
-                # neg diagonal is occupied
-                or neg_diagonal in self.occupied_neg_diagonal
+                or
+                pos_idx in self.occupied_pos_diag
+                or
+                neg_idx in self.occupied_neg_diag
             ):
+                # skip this combination
                 continue
 
             # mark as occupied
             self.occupied_cols.add(col)
-            self.occupied_pos_diagonal.add(pos_diagonal)
-            self.occupied_neg_diagonal.add(neg_diagonal)
+            self.occupied_pos_diag.add(pos_idx)
+            self.occupied_neg_diag.add(neg_idx)
 
-            # check this place
+            # check next row
             self.board[row][col] = "Q"
             self.backtrack(row + 1)
             self.board[row][col] = "."
 
-            # unmark as occupied
+            # free space
             self.occupied_cols.remove(col)
-            self.occupied_pos_diagonal.remove(pos_diagonal)
-            self.occupied_neg_diagonal.remove(neg_diagonal)
+            self.occupied_pos_diag.remove(pos_idx)
+            self.occupied_neg_diag.remove(neg_idx)
 
     def totalNQueens(self, n: int) -> int:
         self.n = n
-        # col
+        # cols
         self.occupied_cols = set()
         # row + col
-        self.occupied_pos_diagonal = set()
+        self.occupied_pos_diag = set()
         # row - col
-        self.occupied_neg_diagonal = set()
+        self.occupied_neg_diag = set()
 
         self.board = [["."] * n for _ in range(n)]
         self.res = 0
